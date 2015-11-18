e032b026f0f1a3861b9fd1c7141f0f74c5bb3d66
Fabio Rehm <fgrehm@gmail.com>
2015-11-18 20:37:11 -0200

    Do not allow inserting a key that already exists

--- src/simplejsondb/actions/insert.go
+++ src/simplejsondb/actions/insert.go
@@ -1,6 +1,7 @@
 package actions
 
 import (
+	"fmt"
 	"simplejsondb/core"
 	"simplejsondb/dbio"
 )
@@ -8,6 +9,11 @@ import (
 func Insert(index core.Uint32Index, buffer dbio.DataBuffer, record *core.Record) error {
 	cb := core.NewDataBlockRepository(buffer).ControlBlock()
 	buffer.MarkAsDirty(cb.DataBlockID())
+
+	rowID, _ := index.Find(record.ID)
+	if rowID != (core.RowID{}) {
+		return fmt.Errorf("Key already exists: %d", record.ID)
+	}
 
 	allocator := core.NewRecordAllocator(buffer)
 	rowID, err := allocator.Add(record)
